34afab317ea12c010fbc81ac5d322a3b8db8053f
user <user@DESKTOP-T3CBQT9>
2023-02-26 00:37:30 +0400

    beta

--- include/Function1/Function1.h
+++ include/Function1/Function1.h
@@ -1,3 +1,55 @@
 #pragma once
 
-int sum_stub(int lhs, int rhs);
+namespace function {
+
+	const float E = 2.718f;
+
+	enum class Type {
+		log,
+		power
+
+	};
+
+	class Function {
+	private:
+		Type _type;
+		float _a;
+		float _b;
+		float _c;
+
+		Function(Type type, float a);
+
+	public:
+		static Function create_log(float a);
+		static Function create_power(float a);
+
+		Function();
+		Type get_type() const;//получать тип
+		float get_a() const;//получать а
+		float get_c() const;
+		float compute_value(float x, float b) const;//вфчислить значение
+
+		Function compute_derivative() const;//вычислить производную
+
+	};
+
+	bool operator==(const Function& Ihs, const Function rhs);
+	bool operator!=(const Function& Ihs, const Function rhs);
+
+
+	class Functionlist {
+	public:
+		static const int CAPACITY = 10;
+	private:
+		Function _function[CAPACITY];
+		int _size;
+	public:
+		Functionlist();
+		int size() const;
+		Function operator[](int index) const;
+		void add(Function f);
+	};
+
+	int index_of_min_value(const Functionlist& function, float x);
+
+}
--- src/Function1.cc
+++ src/Function1.cc
@@ -1,5 +1,54 @@
 #include <Function1/Function1.h>
 
-int sum_stub(int lhs, int rhs) {
-    return lhs + rhs;
+#include <cassert>
+#include <cmath>
+#include <stdexcept>
+
+using namespace function;
+using namespace std;
+
+Function Function::create_log(const float a) {
+    return Function(Type::log, a);
+}
+
+Function Function::create_power(const float a) {
+    return Function(Type::power, a);
+}
+Function::Function() : _type(Type::log), _a(1) { }
+
+Function::Function(const Type type, const float a) : _type(type), _a(a) { }
+
+Type Function::get_type() const {
+    return _type;
+}
+
+float Function::get_a() const {
+    return _a;
+
+
+}
+float Function::get_c() const {
+    return _c;
+}
+
+float Function::compute_value(const float x, const float b) const {
+    switch (_type) {
+    case Type::log:
+        return _c * log(x);
+    case Type::power:
+        return _a * pow(x, b);
+    default:
+        throw runtime_error("[Function::compute_value] Invalid function type.");
+    }
+}
+
+Function Function::compute_derivative() const {
+    switch (_type) {
+    case Type::log:
+        return create_log(1/_a);
+    case Type::power:
+        return create_power(_b*_a^(_b-1));
+    default:
+        throw runtime_error("[Function::compute_derivative] Invalid function type.");
+    }
 }
